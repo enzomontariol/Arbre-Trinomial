262852bd43a23a61e882f49cb6569395876b7baa
Zozom65 <109828140+Zozom65@users.noreply.github.com>
2024-10-23 18:00:47 +0200

    Suppressions des classes inutilisées

--- Python/Classes/module_enums.py
+++ Python/Classes/module_enums.py
@@ -8,14 +8,3 @@ class ConventionBaseCalendaire(Enum) :
     _365 = 365
     _360 = 360
     
-class MethodeConstructionArbre(Enum) : 
-    vanille = "vanille"
-    speed = "speed"
-    
-class OptionTypeBS (Enum) : 
-    call = "call"
-    put = "put"
-
-class ExerciseType (Enum) : 
-    europeen = "europeen"
-    americain = "americain"
