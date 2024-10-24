bfb1c419d69fdaee53f2795c9b5d2d741e1487f2
Zozom65 <109828140+Zozom65@users.noreply.github.com>
2024-10-24 17:12:27 +0200

    Ajout de la feature barrière au graph

--- Python/Classes/module_graphique.py
+++ Python/Classes/module_graphique.py
@@ -3,8 +3,9 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 from matplotlib.lines import Line2D
+import numpy as np
     
-from Python.Classes.module_arbre_noeud import Arbre
+from module_arbre_noeud import Arbre
   
   
 #%% Classes  
@@ -26,33 +27,41 @@ class Arbre_Graph :
             
         if self.arbre.nb_pas > 10 : 
             raise ValueError("Nombre de pas dans l'arbre trop important pour être affiché. Veuillez choisir un nombre de pas inférieur à 10.")
-
     def afficher_arbre(self) -> None:
-        """Fonction nous permettant de réaliser le graphique
-        """
+        """Fonction nous permettant de réaliser le graphique en positionnant les nœuds selon le prix sous-jacent."""
         
-        #Initialisation de l'objet issu de la librairie networkx
+        # Initialisation de l'objet issu de la librairie networkx
         G = nx.DiGraph()
         labels = {}
         positions = {}
-        queue = [(self.arbre.racine, 0, 0)]
+        queue = [(self.arbre.racine, 0)]
+        
+        # Access the barrier level from the option data
+        niveau_barriere = self.arbre.option.barriere.niveau_barriere
+
+        # Déterminer le prix max et min des sous-jacents pour ajuster l'échelle
+        prix_min, prix_max = float('inf'), float('-inf')
         
         while queue:
-            noeud, x, y = queue.pop(0)
+            noeud, x = queue.pop(0)
             
-            # Création d'un label pour chaque noeuds:
-            # 1. valeur_intrinseque
-            # 2. prix_sj
-            # 3. p_cumule
+            # Utilisation du prix sous-jacent pour la coordonnée y
+            y = noeud.prix_sj
+
+            # Mise à jour des limites des prix
+            prix_min = min(prix_min, y)
+            prix_max = max(prix_max, y)
+            
+            # Création d'un label pour chaque nœud : valeur_intrinseque, prix_sj, p_cumule
             noeud_label = f"{noeud.valeur_intrinseque:.2f}\n{noeud.prix_sj:.2f}\n{noeud.p_cumule:.6f}"
             labels[noeud] = noeud_label
             positions[noeud] = (x, y)
             G.add_node(noeud)
             
-            # Nous iterons sur chaque futur noeud
+            # Nous itérons sur chaque futur nœud (bas, centre, haut)
             for direction, futur in zip(["bas", "centre", "haut"], [noeud.futur_bas, noeud.futur_centre, noeud.futur_haut]):
                 if futur is not None:
-                    # Nous prenons la probabilité correspondante à chaque noeud
+                    # Probabilité correspondante à chaque direction
                     if direction == "bas":
                         prob = noeud.p_bas
                     elif direction == "centre":
@@ -65,38 +74,73 @@ class Arbre_Graph :
                     # Formatage de la probabilité
                     prob_label = f"{prob:.4f}"
                     
-                    # Nous ajoutons une ligne avec la proba en label
+                    # Ajout de la ligne avec la probabilité en label
                     G.add_edge(noeud, futur, label=prob_label)
                     
-                    # Ajout du noeud futur à la queue pour itérer dessus 
-                    queue.append((futur, x + 1, y - 1 if direction == "bas" else y + 1 if direction == "haut" else y))
+                    # Ajout du nœud futur à la queue pour itérer dessus 
+                    queue.append((futur, x + 1))
         
         plt.figure(figsize=self.dimension_chart)
         
-        # graph le noeud avec le label correspondant
+        # Ajuster les limites de l'axe des y selon les prix sous-jacents
+        y_margin = (prix_max - prix_min) * 0.05  # Ajout d'une marge de 10% en haut et en bas
+        plt.ylim(prix_min - y_margin, prix_max + y_margin)
+
+        # Dessiner les nœuds et leurs labels
         nx.draw_networkx_nodes(G, pos=positions, node_size=2500, node_color='lightblue')
         nx.draw_networkx_labels(G, pos=positions, labels=labels, font_size=10)
         
-        # extraction des labels des lignes
+        # Extraction des labels des lignes
         edge_labels = nx.get_edge_attributes(G, 'label')
         nx.draw_networkx_edges(G, pos=positions, arrows=True, arrowstyle='-|>', arrowsize=20)
         nx.draw_networkx_edge_labels(G, pos=positions, edge_labels=edge_labels, font_color='red', font_size=8, label_pos=0.3)
-
+        
+        # Ajout d'une ligne horizontale pour représenter la barrière à son niveau correct
+        plt.axhline(y=niveau_barriere, color='green', linestyle='--', label=f'Barrière : {niveau_barriere:.2f}')
+        
         # Ajout d'un titre au graphique
-        plt.title("Arbre Trinomial", fontsize=16)
+        
+        if self.arbre.option.call : 
+            type_option = "call"
+        else : 
+            type_option = "put"
+            
+        if self.arbre.option.americaine : 
+            exercice_option = "américaine"
+        else : 
+            exercice_option = "européenne"
+            
+        strike = self.arbre.option.prix_exercice
+        
+        barriere_titre=""
+        
+        if self.arbre.option.barriere.direction_barriere : 
+            barriere_titre = f", barrière {self.arbre.option.barriere.type_barriere.value} {self.arbre.option.barriere.direction_barriere.value} {self.arbre.option.barriere.niveau_barriere}"
+        
+        plt.title(f"Arbre Trinomial, option {type_option} {exercice_option}, strike {strike}{barriere_titre}", fontsize=16)
 
         # Ajout d'une légende
-        legend_elements = [
-            Line2D([0], [0], marker='o', color='w', label='Noeud:',
-                markerfacecolor='lightblue', markersize=15),
-            Line2D([0], [0], label="Valeur intrinsèque (Haut)", color='w'),
-            Line2D([0], [0], label="Prix Sous-Jacent (Centre)", color='w'),
-            Line2D([0], [0], label="Probabilité cumulée (Bas)", color='w'),
-            Line2D([0], [0], color='red', lw=2, label='Probabilité de passage')
-        ]
+        if self.arbre.option.barriere.direction_barriere : 
+            legend_elements = [
+                Line2D([0], [0], marker='o', color='w', label='Noeud:',
+                    markerfacecolor='lightblue', markersize=15),
+                Line2D([0], [0], label="Valeur intrinsèque (Haut)", color='w'),
+                Line2D([0], [0], label="Prix Sous-Jacent (Centre)", color='w'),
+                Line2D([0], [0], label="Probabilité cumulée (Bas)", color='w'),
+                Line2D([0], [0], color='red', lw=2, label='Probabilité de passage'),
+                Line2D([0], [0], color='green', lw=2, linestyle='--', label=f'Barrière : {niveau_barriere:.2f}')
+            ]
+        else : 
+            legend_elements = [
+                Line2D([0], [0], marker='o', color='w', label='Noeud:',
+                    markerfacecolor='lightblue', markersize=15),
+                Line2D([0], [0], label="Valeur intrinsèque (Haut)", color='w'),
+                Line2D([0], [0], label="Prix Sous-Jacent (Centre)", color='w'),
+                Line2D([0], [0], label="Probabilité cumulée (Bas)", color='w'),
+                Line2D([0], [0], color='red', lw=2, label='Probabilité de passage')
+            ]
 
         plt.legend(handles=legend_elements, loc='upper left', fontsize=10)
         
-        # plt.axis('off')  # On cache les axes
         plt.tight_layout()
         plt.show()
