2d1d8c7aa71a844ad1ed108853591aaccbb4aa30
Zozom65 <109828140+Zozom65@users.noreply.github.com>
2024-11-02 19:03:30 +0100

    Update run_app.bat

--- Python/run_app.bat
+++ Python/run_app.bat
@@ -1,6 +1,8 @@
 @echo off
-cd /d "%~dp0"
-call .\.venv\Scripts\activate
-streamlit run app.py
+cd /d "%~dp0" || exit /b
+python -m venv .venv || exit /b
+call .\.venv\Scripts\activate || exit /b
+pip install -r requirements.txt || exit /b
+streamlit run app.py || exit /b
 deactivate
 pause
