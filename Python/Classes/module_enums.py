d64b6df84c7bdf82464d271b5d35ec15c15678af
lincecile <lincecile7@gmail.com>
2024-11-02 23:30:53 +0100

    base additionnelle

--- Python/Classes/module_enums.py
+++ Python/Classes/module_enums.py
@@ -7,7 +7,10 @@ from enum import Enum
 class ConventionBaseCalendaire(Enum) :
     _365 = 365
     _360 = 360
-    
+    _252 = 252
+    _257 = 257
+    _366 = 366
+
 class TypeBarriere(Enum) : 
     knock_in = "Knock-in"
     knock_out = "Knock-out"
